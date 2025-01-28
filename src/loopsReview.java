a92aeafc090b2f509b0a1dbe24e9fd3accf14c82
csproles <c.sproles2005@gmail.com>
2025-01-28 12:04:28 -0600

    made it to where you can enter numbers and made the start for the average

--- src/loopsReview.java
+++ src/loopsReview.java
@@ -1,11 +1,26 @@
 package src;
 
+import java.util.Scanner;
+
 public class loopsReview {
     public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int count = 0;
+        double sum = 0;
         double grade = 0;
 
-//        while(grade >= 0){
-//
-//        }
+        while(grade >= 0){
+            System.out.println("Please enter a grade from 0 to 110 (Enter -1 to terminate)");
+            grade = input.nextDouble();
+
+            if(grade > 110){
+                System.out.println("Invalid number, try again");
+            }
+
+            count++;
+            sum += grade;
+        }
     }
 }
